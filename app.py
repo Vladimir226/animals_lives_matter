1601aa5014d19d662989b7dcbc88dc7a4c07954a
Vladimir226 <vovanbotan22@gmail.com>
2022-11-30 19:29:45 +0300

    Создана страница всех приёмов и страница с формой записи на прием

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ def index():
 
     return render_template("index.html")
 
+@app.route('/admissions')
+def admissions():
+    return render_template("admissions.html")
+
+@app.route('/add_admission')
+def add_admission():
+    return render_template('add_admission.html')
+
 if __name__ =="__main__":
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   
+    
+    $("#new-zoo-check").on("click", function () {
+        if ($(this).is(":checked")) {
+            $('.new-zoo-container').css("display", "block");
+            $('.choose-zoo-container').css("display", "none");
+           
+        } 
+        else {
+            $('.new-zoo-container').css("display", "none");
+            $('.choose-zoo-container').css("display", "block");
+           
+        }
+    })
+
 })
